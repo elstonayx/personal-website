e71634bea2be9e00b1b140fefdb29867176e5134
elstonayx <elston.ayx@u.nus.edu>
2019-03-10 17:35:31 +0800

    Add new profilepic component

---
+++ src/components/profilepic.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const ProfilePicture = styled.img`
+  border: 3px solid white;
+  height: 200px;
+  width: 200px;
+  border-radius: 100px;
+`
+export default ProfilePicture
