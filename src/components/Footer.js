e9a0bb549ca6f8e16357a59dd7aaa562fcf139d4
elstonayx <elston.ayx@u.nus.edu>
2019-03-10 17:35:22 +0800

    Update page content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,52 @@
 import React from 'react'
 
 class Footer extends React.Component {
-    render() {
-        return (
-            <section id="footer">
-                <ul className="icons">
-                    <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                    <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                    <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                    <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                    <li><a href="#" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
-                </ul>
-                <ul className="copyright">
-                    <li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-                </ul>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <section id="footer">
+        <ul className="icons">
+          <li>
+            <a
+              href="https://twitter.com/elston_ayx"
+              className="icon alt fa-twitter"
+            >
+              <span className="label">Twitter</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/elstonayx/"
+              className="icon alt fa-linkedin"
+            >
+              <span className="label">LinkedIn</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/elstonayx"
+              className="icon alt fa-github"
+            >
+              <span className="label">GitHub</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:elstonaw@comp.nus.edu.sg"
+              className="icon alt fa-envelope"
+            >
+              <span className="label">Email</span>
+            </a>
+          </li>
+        </ul>
+        <ul className="copyright">
+          <li>&copy; ELSTON AW YU XUAN</li>
+          <li>
+            Design: <a href="http://html5up.net">HTML5 UP</a>
+          </li>
+        </ul>
+      </section>
+    )
+  }
 }
 
 export default Footer
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,33 @@
 import React from 'react'
-
+import ProfilePicture from './profilepic'
+import profilepic from '../assets/images/profilepic.jpg'
 class Header extends React.Component {
-    render() {
-        return (
-            <section id="header">
-                <div className="inner">
-                    <span className="icon major fa-cloud"></span>
-                    <h1>Hi, I'm <strong>Photon</strong>, another fine<br />
-                    little freebie from <a href="http://html5up.net">HTML5 UP</a>.</h1>
-                    <p>Accumsan feugiat mi commodo erat lorem ipsum, sed magna<br />
-                    lobortis feugiat sapien sed etiam volutpat accumsan.</p>
-                    <ul className="actions">
-                        <li><a href="#one" className="button scrolly">Discover</a></li>
-                    </ul>
-                </div>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <section id="header">
+        <div className="inner">
+          <ProfilePicture src={profilepic} />
+          <h1>
+            ELSTON (YU XUAN) <strong>AW</strong>
+          </h1>
+          <p>
+            I'm currently a Year 2 Computer Engineering student
+            <br />
+            studying in the National University of Singapore, and
+            <br />
+            my focus area is on full stack development.
+          </p>
+          <ul className="actions">
+            <li>
+              <a href="#one" className="button scrolly">
+                Discover
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    )
+  }
 }
 
 export default Header
