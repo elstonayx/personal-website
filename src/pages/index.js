620df37b454c88483623470225b3926736eeb6cd
elstonayx <elston.ayx@u.nus.edu>
2019-04-02 20:58:41 +0800

    Update Experience column

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
-import 'react-sweet-progress/lib/style.css'
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react'
 
 import hopposter from '../assets/images/hopposter.jpg'
 import vincent from '../assets/images/vincent.jpg'
@@ -48,11 +48,15 @@ class Homepage extends React.Component {
               <header className="major">
                 <h2>My Education</h2>
               </header>
-              <p>
-                I'm currently studying at National University of Singapore, with
-                a Bachelor of Engineering in Computer Engineering with Honours.
-                Previously, I was studying at Anglo-Chinese Junior College.
-              </p>
+              <div className="smallDiv">
+                <h2>National University of Singapore</h2>
+                <h4>B.Eng in Computer Engineering - Class of 2021</h4>
+                <br />
+                <p>
+                  Pursuing a Computer Engineering degree in the National
+                  University of Singapore.
+                </p>
+              </div>
             </div>
           </div>
         </section>
@@ -64,120 +68,126 @@ class Homepage extends React.Component {
                 <h2>My Experiences</h2>
               </header>
             </div>
-            <div className="smallDiv col-6">
-              <h2>Mobbin</h2>
-              <p>Software Engineering Intern</p>
-              <p>Mar 2019 - present</p>
-            </div>
-            <div className="smallDiv col-6">
-              <p>
-                Mobbin is a hand-picked collection of the latest mobile design
-                patterns from apps that reflect the best in design. It helps
-                designers gain inspiration from other iOS applications and
-                design patterns.
-              </p>
-              <br />
-              <b>Key Job Scope</b>
-              <ul>
-                <li>
-                  Created front-end assets in React to designer’s
-                  specifications.
-                </li>
-                <li>
-                  Assisted with the migration of data from the old codebase to
-                  the new codebase.
-                </li>
-              </ul>
-              <br />
-              <br />
-            </div>
-            <div className="smallDiv col-6">
-              <h2>National University of Singapore</h2>
-              <p>Teaching Assistant for CG2271: Real-time Operating Systems</p>
-              <p>Jan 2019 - present</p>
-            </div>
-
-            <div className="smallDiv col-6">
-              <p>
-                CG2271 teaches students basic realtime operating systems (RTOS)
-                concepts like tasks, scheduling algorithms, RTOS customisation
-                and concurrent real-time programming. Students would gain both
-                conceptual and hands-on knowledge on customizing RTOSes and
-                writing real-time programs.
-              </p>
-              <br />
-              <b>Key Job Scope</b>
-              <ul>
-                <li>
-                  Conduct weekly lab sessions for students, teaching them how to
-                  apply RTOS concepts.
-                </li>
-                <li>
-                  Mark lab reports biweekly, giving timely feedback for
-                  students.
-                </li>
-                <li>
-                  Conduct tutorial sessions to reinforce on concepts taught
-                  during lecture.
-                </li>
-              </ul>
-              <br />
-              <br />
-            </div>
-            <div className="smallDiv col-6">
-              <h2>National University of Singapore</h2>
-              <p>
-                Teaching Assistant for CG1111: Engineering Principles and
-                Practices I
-              </p>
-              <p>Aug - Nov 2018</p>
-            </div>
-
-            <div className="smallDiv col-6">
-              <p>
-                CG1111 is an introductory project module on the engineering
-                principles of how computer-aided systems work and fail, and the
-                engineering practice of how they are designed, built and valued.
-              </p>
-              <br />
-              <b>Key Job Scope</b>
-              <ul>
-                <li>Assist students through weekly lab work.</li>
-                <li>Mark lab reports, giving timely feedback for students.</li>
-                <li>Ensure the safety of the experiments being conducted.</li>
-              </ul>
-              <br />
-              <br />
-            </div>
-            <div className="smallDiv col-6">
-              <h2>Arvato Digital Services</h2>
-              <p>Transactional Specialist</p>
-              <p>Feb 2017 - Jul 2017</p>
-            </div>
-            <div className="smallDiv col-6">
-              <p>
-                Arvato Digital Services is a leading service provider, providing
-                managed BPO services to Microsoft.
-              </p>
-              <br />
-              <b>Key Job Scope</b>
-              <ul>
-                <li>
-                  Handled large transactions for a major global technological
-                  company.
-                </li>
-                <li>
-                  Used Customer Relations Management tools and in-house
-                  management tools to communicate with clients and handle
-                  transactions.
-                </li>
-                <li>
-                  Took part in the annual closing in June 2017, to close many
-                  other major deals with partners.
-                </li>
-              </ul>
-              <br />
-              <br />
+            <div className="col-12">
+              <Timeline lineColor={'#ddd'}>
+                <TimelineItem
+                  key="001"
+                  dateText="March 2019 – Present"
+                  style={{ color: '#e86971' }}
+                >
+                  <h2>Mobbin</h2>
+                  <h4>Front-end Software Engineering Intern</h4>
+                  <br />
+                  <p>
+                    Mobbin is a hand-picked collection of the latest mobile
+                    design patterns from apps that reflect the best in design.
+                    It helps designers gain inspiration from other iOS
+                    applications and design patterns.
+                  </p>
+                  <b>Key Job Scope</b>
+                  <ul>
+                    <li>
+                      Created front-end assets in React to designer’s
+                      specifications.
+                    </li>
+                    <li>
+                      Assisted with the migration of data from the old codebase
+                      to the new codebase.
+                    </li>
+                  </ul>
+                </TimelineItem>
+                <TimelineItem
+                  key="002"
+                  dateText="Jan 2019 – Present"
+                  dateInnerStyle={{ background: '#61b8ff' }}
+                >
+                  <h2>National University of Singapore</h2>
+                  <h4>
+                    Teaching Assistant for CG2271: Real-time Operating Systems
+                  </h4>
+                  <br />
+                  <p>
+                    CG2271 teaches students basic realtime operating systems
+                    (RTOS) concepts like tasks, scheduling algorithms, RTOS
+                    customisation and concurrent real-time programming. Students
+                    would gain both conceptual and hands-on knowledge on
+                    customizing RTOSes and writing real-time programs.
+                  </p>
+                  <b>Key Job Scope</b>
+                  <ul>
+                    <li>
+                      Handled large transactions for a major global
+                      technological company.
+                    </li>
+                    <li>
+                      Used Customer Relations Management tools and in-house
+                      management tools to communicate with clients and handle
+                      transactions.
+                    </li>
+                    <li>
+                      Took part in the annual closing in June 2017, to close
+                      many other major deals with partners.
+                    </li>
+                  </ul>
+                </TimelineItem>
+                <TimelineItem
+                  key="003"
+                  dateText="Aug – Nov 2018"
+                  dateInnerStyle={{ background: '#76bb7f' }}
+                >
+                  <h2>National University of Singapore</h2>
+                  <h4>
+                    Teaching Assistant for CG1111: Engineering Principles and
+                    Practices I
+                  </h4>
+                  <br />
+                  <p>
+                    CG1111 is an introductory project module on the engineering
+                    principles of how computer-aided systems work and fail, and
+                    the engineering practice of how they are designed, built and
+                    valued.
+                  </p>
+                  <b>Key Job Scope</b>
+                  <ul>
+                    <li>Assist students through weekly lab work.</li>
+                    <li>
+                      Mark lab reports, giving timely feedback for students.
+                    </li>
+                    <li>
+                      Ensure the safety of the experiments being conducted.
+                    </li>
+                  </ul>
+                </TimelineItem>
+                <TimelineItem
+                  key="004"
+                  dateText="Feb – Jul 2017"
+                  dateInnerStyle={{ background: '#fcd307' }}
+                >
+                  <h2>Arvato Digital Services</h2>
+                  <h4>Transactional Specialist</h4>
+                  <br />
+                  <p>
+                    Arvato Digital Services is a leading service provider,
+                    providing managed BPO services to Microsoft.
+                  </p>
+                  <b>Key Job Scope</b>
+                  <ul>
+                    <li>
+                      Handled large transactions for a major global
+                      technological company.
+                    </li>
+                    <li>
+                      Used Customer Relations Management tools and in-house
+                      management tools to communicate with clients and handle
+                      transactions.
+                    </li>
+                    <li>
+                      Took part in the annual closing in June 2017, to close
+                      many other major deals with partners.
+                    </li>
+                  </ul>
+                </TimelineItem>
+              </Timeline>
             </div>
           </div>
         </section>
